07f614eae45803e6a6e8e4180fa68588b8189764
Ryan <mukalazironnie@live.com>
2026-06-21 19:22:33 +0300

    Elfa

--- settings.js
+++ settings.js
@@ -6,11 +6,11 @@ if (fs.existsSync('set.env')) {
     require('dotenv').config({ path: __dirname + '/set.env' });
 }
 
-const session = process.env.SESSION || '';
+const session = process.env.SESSION || 'KEITH;;;H4sIAAAAAAAAA5VUW5OiOBT+L3nVGhEElKquWkQExOYiXtCtfUhDwMjVEFCY8r9vYU9Pz8PubC9PIUmd853vku8gL3CFTNQC6TsoCW4gRf2StiUCEpjXUYQIGIIQUggkUJkqKqba2UFNLJ/U5jJTDnQZKfLG4tbpdQ392+j1sG6KpfwCHkNQ1m8pDn5T0FyiqVmwIz1pko26ygYiexj4wuI63R/ss6vO0lEaVfrrSalewKOvCDHBeayWZ5QhAlMTtQ7E5GvwjTVvaTMjycVrgHfKTDejDetok411GYirO1E5+dgI2ZjP1a/B97W3uc/y8mWbzEO+FLaJfDtODu3+XG+L4FLP2+AWe6OJkOze4Vc4zlFohCinmLZf5t20KeuSWTHrplVV8udX3xMHi+oIbSNPV11HSRLlIU6Jn3wNeKpso8jEVjm5+rvY5LhcreF4+7YxF03RzuW5sakKfecy2PgVuEM+vJL8H949GSfdynbUQ2JtboklVttL6C2jZGvky/nViJttqtQDqu2Mr8Fn0rS7TbhrgLhuZqjq6XVAJqN1HhoCtkgWl8nhVe+YN18vPuFDWpPfoYwqX2/drBQ7u6jHHmeeVqf7wcmOYYc8HPDWukDHLD66TOOVs+DY7Zuxl67a+KpfPGox232n6k7XHkfWfpVtC+rhVr69PCdKUGuEQBo/hoCgGFeUQIqL/Lk3BDBsPBQQRJ/kgnOd8MjInaN+YOzO0++lXBq7q7vOoyK2RUE7L1FrL1dHJXgBQ1CSIkBVhUIdV7Qg7SuqKhijCkh//jUEObrTd9n6Ztx4CCJMKrrL6zItYPih6cchDIKizqnX5oHSLxABEvO5jSjFeVz1LNY5JMEZN0g5Q1oBKYJphX7OhwgKgURJjX5mVinCnnZPZzjT4aZgCLKnHDgEEmB5QZzwjMAJ3Fhi+T+qb7e+LCzLbzmiYAjS57WpIDLsTJzwU2HGif3Ffv/xE2BfL0QU4rQCElBs2FzDqabaijGxVE2TjVhWYhl8DvThi3fm2zHiTu54IyOZdS9LeTrAc281ZfflVElnzvG8kuvc3ZzYLnn5hyJAAporp0Jzbjfl2ua90g4FTah5snG8zVhsdK/TrqPFJDs2o+5y16xgNCAnQzC3KL6L+zoTLm+DYCl2m9r2x1ZhaUc/Os7dl75biBocoF+bheP4ZI/nCz9aKKxDilZltwO73rB1Y10MnsXBxKzVA7tr5aZ1MvkusK6M5mgfOgkbnE5rs/EEevHPg1vzpgyipbsiC/fdsc/EpD9eKvx0Uy9V/xth9Ax+DnsB/1u6d+C9w5jH8JcaP56Sf4njPKA+ZXfWztVjKzHuruMU8Q5HVgojuYy2rOG565JRD2HFg8fjryEoU0ijgmRAAjAPSYFDMASkqHvLGnlU/KaZIleGGsdWP3kKKyp/xmCLM1RRmJVAGotTluEFZsIPQdbKZelRSD/SA+T+W5cUPP4G2YLLd1cHAAA=';
 const dev = process.env.OWNER_NUMBER || '254748387615';
 
 const autostatusAutoviewStatus = process.env.AUTOVIEW_STATUS || 'true';
-const autostatusAutoLikeStatus = process.env.AUTOLIKE_STATUS || 'false';
+const autostatusAutoLikeStatus = process.env.AUTOLIKE_STATUS || 'true';
 const autostatusAutoReplyStatus = process.env.AUTOREPLY_STATUS || 'false';
 const autostatusStatusReplyText = process.env.STATUS_REPLY_TEXT || '✅ Status Viewed By Keith Md';
 const autostatusStatusLikeEmojis = process.env.STATUS_LIKE_EMOJIS || '🫟,🫟';
@@ -23,7 +23,7 @@ const botGurl = process.env.BOT_GURL || "https://github.com/Keithkeizzah/KEITH-M
 const botTimezone = process.env.BOT_TIMEZONE || "Africa/Nairobi";
 const botBotname = process.env.BOTNAME || "KEITH-MD";
 const botPackname = process.env.BOT_PACKNAME || "KEITH-MD";
-const botMode = process.env.BOT_MODE || "public";
+const botMode = process.env.BOT_MODE || "private";
 const botSessionName = process.env.BOT_SESSION_NAME || "keith-md";
 const autosocialdownload = process.env.AUTO_SOCIAL_DOWNLOAD || "false";
 
